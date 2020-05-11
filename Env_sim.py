ea0cfa9daa8e7fadbde3e4fe2a5c24472b8a826e
Vidyaa <42498128+vidyaa29@users.noreply.github.com>
2020-05-11 12:37:39 +0530

    Add files via upload

---
+++ Env_sim.py
@@ -0,0 +1,45 @@
+import gym
+import envs
+import time
+from matplotlib import pyplot as plt
+#import cv2
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to import cv2 under python3
+import cv2
+sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
+env = gym.make('CustomEnv-v0')
+n=0
+
+while True:
+    #env.step()
+    plt.imshow(env.render(mode='rgb_array'))
+
+    k=cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    elif k == ord('w'):
+        if env.agent[n].agent_pos[0]>0 and env.agent[n].map_check(env.agent[n].agent_pos[0]-1,env.agent[n].agent_pos[1])!=0 :
+            env.agent[n].odom_update( [env.agent[n].agent_pos[0]-1,env.agent[n].agent_pos[1]] )
+    elif k == ord('s'):
+        if env.agent[n].agent_pos[0]+1<env.size[0] and env.agent[n].map_check(env.agent[n].agent_pos[0]+1,env.agent[n].agent_pos[1])!=0 :
+            env.agent[n].odom_update( [env.agent[n].agent_pos[0]+1,env.agent[n].agent_pos[1]] )
+    elif k == ord('a'):
+        if env.agent[n].agent_pos[1]>0 and env.agent[n].map_check(env.agent[n].agent_pos[0],env.agent[n].agent_pos[1]-1)!=0 :
+            env.agent[n].odom_update( [env.agent[n].agent_pos[0],env.agent[n].agent_pos[1]-1] )
+    elif k == ord('d'):
+        if env.agent[n].agent_pos[1]+1<env.size[1] and env.agent[n].map_check(env.agent[n].agent_pos[0],env.agent[n].agent_pos[1]+1)!=0 :
+            env.agent[n].odom_update( [env.agent[n].agent_pos[0],env.agent[n].agent_pos[1]+1] )
+    elif k == ord('f') :
+        env.agent[n].step()
+    elif k == ord('m') :
+        for x in env.largest_contour:
+		        env.agent[n].odom_update([x(0),x(1)])
+		        env.render()
+		        if cv2.waitKey(1) & 0xFF == ord('x'):
+				        break
+    elif k >= ord('0') and k <= ord('9'):
+        _n=k-ord('0')
+        if _n< env.agents_count:
+                n=_n
+
+env.close()
